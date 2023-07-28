c24072aeeaf7ebaeba711300472bd15e11c1e592
SheikhArbab <arbabzafar4444@gmail.com>
2023-07-28 07:36:02 +0500

    0.2

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,30 +2,55 @@ let character = document.querySelector('#character');
 let block = document.querySelector('#block');
 
 document.querySelector('#jumpeBtn').addEventListener('click', () => {
-
-    character.classList.add('jumpe')
-    
-    setTimeout(() => {
-        
-        
-        character.classList.remove('jumpe')
-
-    
-        
-    }, 1000);
-
+  character.classList.add('jumpe')
+  setTimeout(() => {
+    character.classList.remove('jumpe')
+  }, 1000);
 });
+// jumpeBtn
+
+window.addEventListener('load', () => {
+  let preLoader = document.querySelector('#preLoader')
+  setTimeout(() => {
+    preLoader.style.height = '0'
+  }, 1000);
+});
+// preLoader
 
 
 
-
-let audioButtons = Array.from(document.getElementsByClassName("audioBtn"));
+let audioButtons = Array.from(document.querySelectorAll(".audioBtn"));
 audioButtons.forEach(function (button) {
-  button.addEventListener("click", playClickSound);
+  button.addEventListener("click", () => {
+    let clickSound = document.querySelector("#clickSound");
+    clickSound.currentTime = 0;
+    clickSound.play();
+  });
 });
 
-function playClickSound() {
-  let clickSound = document.getElementById("clickSound");
-  clickSound.currentTime = 0;
-  clickSound.play();
-}
+// audioBtn
+
+
+
+let checkDead = setInterval(() => {
+  let characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+  let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+  let gameOver = document.querySelector('.gameOver');
+let restart = document.querySelector('#restart');
+
+restart.addEventListener('click', () => {
+  
+  location.reload();
+  gameOver.style.display = 'none'
+
+
+});
+  
+  if (blockLeft < 60 && blockLeft > 0 && characterBottom > 220) {
+    
+    clickSound.play();
+    gameOver.style.display = 'flex'
+    clearInterval(checkDead); // Stop the interval when the condition is met
+  }
+
+}, 0);
